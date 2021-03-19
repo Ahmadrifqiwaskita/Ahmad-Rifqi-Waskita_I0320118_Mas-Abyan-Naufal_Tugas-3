4b30a2b412610d0a73cdb1644caa4dab9bbc44eb
Ahmadrifqiwaskita <80144753+Ahmadrifqiwaskita@users.noreply.github.com>
2021-03-20 06:34:29 +0700

    Add files via upload

---
+++ I0320118_soal1_tugas3.py
@@ -0,0 +1,19 @@
+#Program List
+Nama_Teman = ['Aji','Alvin','Tito','William','Sony','Rizal','Zedi','Rahmat','Raysa','Alya']
+
+print(Nama_Teman[4])
+print(Nama_Teman[6])
+print(Nama_Teman[7])
+
+Nama_Teman[3] = "Usman"
+Nama_Teman[5] = "Rara"
+Nama_Teman[9] = "Titus"
+
+Nama_Teman.append('Yuki')
+Nama_Teman.append('Rizky')
+
+print("Nama Teman:",Nama_Teman)
+print(len(Nama_Teman))
+
+
+
---
+++ I0320118_soal2_tugas3.py
@@ -0,0 +1,18 @@
+#Program Dictionary
+Biodata = {'Nama': 'Rifqi',
+           'Hobi':['Bermain','menonton'],
+           'Sosmed':['IG=@rifqiwskt','Hp=081283545914'],
+           'Lagu_favorit':['To The Bone(Pamungkas)','Rumah ke Rumah(Hindia)'],
+           'Makanan_favorit':['Nasi Goreng','Mi Ayam']}
+
+Biodata['Hobi'] = 'Berkuda','menonton'
+Biodata['Sosmed'] = 'Line=@ahmadrifqiwaskita','Hp=081283545914'
+
+del Biodata['Makanan_favorit']
+
+Biodata['Hobi'] = 'Berkuda','menonton','Olahraga'
+
+print(Biodata)
+
+
+
